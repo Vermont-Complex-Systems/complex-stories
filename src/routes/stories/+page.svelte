8dec20647042896e1cf763a4116906f6fefd5e9b
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-05-22 16:01:00 -0400

    some structure from sveltekit tutorial

---
+++ src/routes/stories/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	let { data } = $props();
+</script>
+
+<h1>stories</h1>
+
+<ul>
+	{#each data.summaries as { slug, title }}
+		<li><a href="/stories/{slug}">{title}</a></li>
+	{/each}
+</ul>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from "path";
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import dsv from "@rollup/plugin-dsv";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [enhancedImages(), sveltekit(), dsv()],
+	resolve: {
+		alias: {
+			$data: path.resolve("./src/data"),
+			$styles: path.resolve("./src/styles"),
+		}
+	}
 });
