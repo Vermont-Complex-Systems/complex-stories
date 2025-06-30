a29ccd7dd7943e5009ae03e5efdb6f29877cc5e9
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-06-30 10:46:24 -0400

    dynamic imports

--- src/lib/stories/open-academic-analytics/data/loader.js
+++ src/lib/stories/open-academic-analytics/data/loader.js
@@ -1,26 +1,15 @@
 // src/lib/stories/open-academic-analytics/data/loader.js
-import paperUrl from './processed/paper.parquet?url';
-import authorUrl from './processed/author.parquet?url';
-import coauthorUrl from './processed/coauthor.parquet?url';
+// Dynamic imports that resolve at runtime
+export async function getDataUrls() {
+  const [paperModule, authorModule, coauthorModule] = await Promise.all([
+    import('./processed/paper.parquet?url'),
+    import('./processed/author.parquet?url'),
+    import('./processed/coauthor.parquet?url')
+  ]);
 
-// Debug: log the URLs to see what we're getting
-console.log('Paper URL:', paperUrl);
-console.log('Author URL:', authorUrl);
-console.log('Coauthor URL:', coauthorUrl);
-
-export const datasets = {
-  paper: {
-    url: paperUrl,
-    description: 'Academic paper publications'
-  },
-  author: {
-    url: authorUrl,
-    description: 'Author information'
-  },
-  coauthor: {
-    url: coauthorUrl,
-    description: 'Coauthor relationships'
-  }
-};
-
-export { paperUrl, authorUrl, coauthorUrl };
+  return {
+    paperUrl: paperModule.default,
+    authorUrl: authorModule.default,
+    coauthorUrl: coauthorModule.default
+  };
+}
