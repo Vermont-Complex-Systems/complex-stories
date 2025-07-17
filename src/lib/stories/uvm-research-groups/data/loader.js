a4d375f48038e23d06dbb21b96003b4e52651575
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-07-17 14:04:34 -0400

    adding college to embedding plot

--- src/lib/stories/uvm-research-groups/data/loader.js
+++ src/lib/stories/uvm-research-groups/data/loader.js
@@ -2,12 +2,17 @@
 import { base } from '$app/paths';
 
 // Use static URLs instead of imports
-export const trainingUrl = `${base}/data/open-academic-analytics/uvm_profs_2023.parquet`;
+export const uvmProfsURL = `${base}/data/open-academic-analytics/uvm_profs_2023.parquet`;
+export const trainingUrl = `${base}/data/open-academic-analytics/training_data.parquet`;
 export const departmentURL = `${base}/data/open-academic-analytics/uvm_departments.parquet`;
 export const coauthorURL = `${base}/data/open-academic-analytics/coauthor.parquet`;
 export const paperURL = `${base}/data/open-academic-analytics/paper.parquet`;
 
 export const datasets = {
+  uvm_profs_2023: {
+    url: uvmProfsURL,
+    description: 'Model output'
+  },
   training: {
     url: trainingUrl,
     description: 'Model output'
