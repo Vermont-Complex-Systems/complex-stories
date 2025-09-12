04fda5fe8f4782cb3f0d881f897c0b849938a422
Jonathan St-Onge <jstonge1@uvm.edu>
2025-09-12 10:36:33 -0400

    moving to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				console.warn(`Skipping ${path}: ${message}`);
-
-			}
-		}
-	}
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
 };
 
 export default config;
